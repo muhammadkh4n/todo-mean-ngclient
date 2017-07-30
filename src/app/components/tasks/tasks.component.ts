e82168b99cce292ed5f130007aa2820b2b722f57
Muhammad Khan <muhammadkh4n@gmail.com>
2017-07-31 00:45:59 +0500

    Initial Commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+  title = 'app';
+}
